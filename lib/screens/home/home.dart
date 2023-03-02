05f61b96232f68ecf2a58f2eaeaf551406de7f86
Sapo-Cururu <118819457+Sapo-Cururu@users.noreply.github.com>
2023-03-02 10:42:03 -0300

    Update home.dart

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
-  Widget build(BuildContext context){
+class Home extends StatelessWidget {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Image.asset("emblem_256.png"),
+        leading: Image.asset("assets/logo.png"),
         title: Text("Charmander #004"),
+        backgroundColor: const Color(0xFFE3350D),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Image.asset("assets/Charmander.png")
-          ],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Image.asset("assets/charmander.png"),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 20),
+                child: Text('Têm preferência por coisas quentes. Quando chove, diz-se que vapor jorra da ponta da cauda.'),
+              ),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      // 1 Linha
+                      Row(
+                        children: [
+                          Column(
+                            children: [
+                              titleText(text: "Altura", context: context),
+                            ],
+                          ),
+                        ],
+                      ),
+                      // 2 Linha
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
+  } // Build
+
+  Text titleText({
+    required String text,
+    required BuildContext context,
+  }) {
+    return Text(text);
   }
 }
