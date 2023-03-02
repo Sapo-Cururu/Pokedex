c823f42d7bb42a588e8fac36ef560190e92275d8
Sapo-Cururu <118819457+Sapo-Cururu@users.noreply.github.com>
2023-03-02 10:42:37 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,15 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Image.asset("assets/logo.png"),
-        title: Text("Charmander #004"),
-        backgroundColor: const Color(0xFFE3350D),
-      ),
-      body: SizedBox(
-        width: MediaQuery.of(context).size.width,
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Image.asset("assets/charmander.png"),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 20),
-                child: Text('Têm preferência por coisas quentes. Quando chove, diz-se que vapor jorra da ponta da cauda.'),
-              ),
-              Card(
-                child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Column(
-                    children: [
-                      // 1 Linha
-                      Row(
-                        children: [
-                          Column(
-                            children: [
-                              titleText(text: "Altura", context: context),
-                            ],
-                          ),
-                        ],
-                      ),
-                      // 2 Linha
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  } // Build
+import 'screens/home/home.dart';
 
-  Text titleText({
-    required String text,
-    required BuildContext context,
-  }) {
-    return Text(text);
-  }
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: const Color(0xFFE3350D),
+    ),
+    title: "Projeto",
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
